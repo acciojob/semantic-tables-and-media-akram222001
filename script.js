75641039f5c058a6ac32bfd1559488fe4a62d223
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-02-22 03:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
 //your code here
-cy.visit(baseUrl + "/main.html");
-cy.contains('List of medias');
-cy.get('ol li:last audio').should('have.attr', 'src', "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg");
